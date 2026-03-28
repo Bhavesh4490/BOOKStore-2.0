a1f8d2aa7bf5d04faa4984be69901d87c59be5fd
Bhavesh <bhaveshrajoriaa@gmail.com>
2026-03-28 14:36:50 +0530

    Create firebase-messaging-sw.js

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,25 @@
+importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+  apiKey: "AIzaSyCcpG-oEqkDprgTwWXKt5CFsttjAQAZFNg",
+  authDomain: "bookstore-6ab08.firebaseapp.com",
+  projectId: "bookstore-6ab08",
+  messagingSenderId: "102226032749",
+  appId: "1:102226032749:web:f7090175d2e53c2f522764"
+});
+
+const messaging = firebase.messaging();
+
+/* 🔔 BACKGROUND NOTIFICATION */
+messaging.onBackgroundMessage(function(payload) {
+  console.log('[SW] Background message:', payload);
+
+  const title = payload.notification.title;
+  const options = {
+    body: payload.notification.body,
+    icon: "/logo.png"
+  };
+
+  self.registration.showNotification(title, options);
+});
